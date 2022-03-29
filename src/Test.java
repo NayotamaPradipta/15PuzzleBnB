9692aab0d14f6e734686957135e7c4f687e9ac21
Nayotama Pradipta <n4y0t4m4@gmail.com>
2022-03-29 16:05:25 +0700

    Add testing file

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import java.io.FileNotFoundException;
+
+public class Test {
+    // Buat testing Tile Class beserta EmptyTile & OccupiedTile
+    public static void main(String[] args) throws FileNotFoundException {
+        Puzzle x = new Puzzle("Reachable2.txt");
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (x.getTile(i, j).getNum() != 16) {
+                    System.out.print(x.getTile(i, j).getNum());
+                    System.out.print(" ");
+                } else {
+                    System.out.print("   ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
